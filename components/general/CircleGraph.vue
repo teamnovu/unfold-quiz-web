f46c0ef3288fa318c229be68be78a335efb84221
theLeroy <janis.wanger@novu.ch>
2022-02-24 17:17:38 +0100

    Added CircleGraph

---
+++ components/general/CircleGraph.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <div class="absolute w-full">
+      <div class="w-full h-full gradient rounded-full aspect-square">asd</div>
+    </div>
+    <div class="-translate-x-4 -translate-y-4">
+      <ul class="sliceWrapper">
+        <li v-for="(question, index) in questionsC" :key="index" :style="{'transform': `rotate(${(index) *slicedeg}deg) skewY(${slicedeg+90}deg);`}">
+          <div class="slice"></div>
+        </li>
+        <!-- <li>
+          <div class="slice"></div>
+        </li> -->
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            questions: 3,
+            questionsC: 1,
+        }
+    },
+    computed: {
+        slicedeg() {
+            return 360 / this.questions
+        }
+    }
+
+ 
+
+};
+</script>
+
+<style scoped>
+.gradient {
+  background: transparent
+    conic-gradient(
+      from 0deg at 50% 50%,
+      #ede9db 0%,
+      #e9e2cb 18.23%,
+      #f0e8cb 33%,
+      #d5c283 56.65%,
+      #d5c283 74.38%,
+      #e5d087 84.73%,
+      #ebdfb7 100%
+    )
+    0% 0% no-repeat padding-box;
+}
+</style>
+
+<style lang="scss">
+
+
+// 360/5=72 -> rotate
+// rotate+90=162 -> skewY
+
+
+
+.sliceWrapper {
+  position: relative;
+  padding: 0;
+  width: 22rem;
+  transform: rotate(315deg);
+  height: 22rem;
+  border-radius: 50%;
+  list-style: none;
+  overflow: hidden;
+}
+
+.slice {
+  position: absolute;
+  left: -100%;
+  width: 200%;
+  height: 200%;
+}
+
+li {
+  overflow: hidden;
+  position: absolute;
+  top: -50%;
+  right: -50%;
+  width: 100%;
+  height: 100%;
+  transform-origin: 0% 100%;
+  border:1px #EEEAD9 solid;
+  background: #EEEAD9;
+}
+
+// @for $i from 1 through 25 {
+//   li:nth-child(#{$i}) {
+//     transform: rotate(calc((#{$i}-1) * var(--slice-deg) + 0deg))
+//       skewY(calc(var(--slice-deg) + 90deg));
+//   }
+// }
+
+// @for $i from 1 through $questionsCorect {
+//   li:nth-child(#{$i}) {
+//     background: #EEEAD9;
+//     border: 1px #EEEAD9 solid;
+//   }
+// }
+</style>
--- pages/Solutions.vue
+++ pages/Solutions.vue
@@ -1,20 +1,26 @@
 <template>
-  <div class="flex justify-between">
-    <div class="w-1/2">
-      <h3
-        class="text-custom-gold-light mb-7 text-xl font-bold sm:mb-9 sm:text-3xl"
-      >
-        12 Punkte!
-      </h3>
-      <h1 class="typo-600">Gratuliere du gehörst zu den novu Profis!!</h1>
-      <p>
-        Hol dir jetzt dein Mandelbärli an unserem Stand ab! Zeig uns dafür deine
-        Punktzahl.
-      </p>
-    </div>
-    <div>
-      <div class="gradient rounded-full" />
-      <div>12/15 Richtig</div>
+  <div
+    class="flex min-h-screen flex-col justify-between px-9 pt-32 pb-9 sm:pl-28 sm:pr-40 sm:pt-56 sm:pb-28"
+  >
+    <div class="flex justify-between">
+      <div class="w-1/2">
+        <h3
+          class="text-custom-gold-light mb-7 text-xl font-bold sm:mb-20 sm:text-3xl"
+        >
+          12 Punkte!
+        </h3>
+        <h1 class="typo-600">Gratuliere du gehörst zu den novu Profis!!</h1>
+        <p class="typo-400 mt-14">
+          Hol dir jetzt dein Mandelbärli an unserem Stand ab! Zeig uns dafür
+          deine Punktzahl.
+        </p>
+      </div>
+      <div>
+        <div class="w-80 relative" >
+          <CircleGraph/>
+        </div>
+        <div>12/15 Richtig</div>
+      </div>
     </div>
   </div>
 </template>
@@ -25,23 +31,14 @@ export default {
     completion() {
       return this.$store.getters.completion
     },
+
+    index() {
+      return this.$store.getters.index - 1
+    },
+
+    amountQuestions() {
+      return this.$store.getters.questions.length
+    },
   },
 }
 </script>
-
-<style scoped>
-.gradient {
-  background: transparent
-    conic-gradient(
-      from 0deg at 50% 50%,
-      #ede9db 0%,
-      #e9e2cb 18.23%,
-      #f0e8cb 33%,
-      #d5c283 56.65%,
-      #d5c283 74.38%,
-      #e5d087 84.73%,
-      #ebdfb7 100%
-    )
-    0% 0% no-repeat padding-box;
-}
-</style>
