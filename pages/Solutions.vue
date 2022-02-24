dc8be5b761c9d3322f339b2094bf968ab4571612
theLeroy <janis.wanger@novu.ch>
2022-02-24 12:41:12 +0100

    Update Solutions.vue

--- pages/Solutions.vue
+++ pages/Solutions.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>sd</div>
+  <div class="flex justify-between">
+    <div class="w-1/2">
+      <h3
+        class="text-custom-gold-light mb-7 text-xl font-bold sm:mb-9 sm:text-3xl"
+      >
+        12 Punkte!
+      </h3>
+      <h1 class="typo-600">Gratuliere du gehörst zu den novu Profis!!</h1>
+      <p>
+        Hol dir jetzt dein Mandelbärli an unserem Stand ab! Zeig uns dafür deine
+        Punktzahl.
+      </p>
+    </div>
+    <div>
+      <div class="gradient rounded-full" />
+      <div>12/15 Richtig</div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,3 +28,20 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.gradient {
+  background: transparent
+    conic-gradient(
+      from 0deg at 50% 50%,
+      #ede9db 0%,
+      #e9e2cb 18.23%,
+      #f0e8cb 33%,
+      #d5c283 56.65%,
+      #d5c283 74.38%,
+      #e5d087 84.73%,
+      #ebdfb7 100%
+    )
+    0% 0% no-repeat padding-box;
+}
+</style>
