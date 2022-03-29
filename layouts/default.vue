c03ad32763d996e45c842a033a69f9e506f0adb8
theLeroy <janis.wanger@novu.ch>
2022-03-29 14:06:05 +0200

    Fix border on mobile

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,10 +15,10 @@
       </transition>
     </div>
     <!-- NUXT -->
-    <Nuxt />
+    <Nuxt class="inset-0 border-4 border-custom-gold-light sm:border-0;"/>
     <!-- BORDER -->
     <div
-      class="pointer-events-none fixed inset-0 border-4 border-custom-gold-light sm:border-6"
+      class="pointer-events-none fixed inset-0 border-0 border-custom-gold-light sm:border-6"
     />
   </div>
 </template>
