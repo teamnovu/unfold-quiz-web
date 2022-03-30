aa9d0e765b80612ca90c39cfd2476e84b445d4de
Manuel Strebel <manuel.strebel@novu.ch>
2022-03-31 00:56:33 +0200

    novu!!!!!!

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   ssr: false,
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Unfold Quiz | Novu',
+    title: 'Unfold Quiz | novu',
     htmlAttrs: {
       lang: 'de',
     },
