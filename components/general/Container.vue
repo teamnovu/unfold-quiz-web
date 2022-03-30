ff7b709071f8e18bb5ae50bf521fcdb40cda7813
theLeroy <janis.wanger@novu.ch>
2022-03-30 18:50:25 +0200

    Btn alignment varies depending on whether you can scroll

--- components/general/Container.vue
+++ components/general/Container.vue
@@ -1,9 +1,47 @@
 <template>
-  <section class="px-9 pt-24 pb-10 sm:pl-28 sm:pr-40 sm:pt-40 ">
+  <section
+    class="px-9 pt-24 sm:pl-28 sm:pr-40 sm:pt-40 pb-8"
+    :class="{ '!pb-24': screenCanScroll }"
+  >
     <slot />
   </section>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      screenCanScroll: false,
+    };
+  },
+
+  destroyed() {
+    window.removeEventListener("resize", this.handleResize);
+  },
+  mounted() {
+    if (process.client) {
+      setTimeout(() => {
+        this.handleResize();
+      }, 500);
+      //Why this next tick thing dont work brrr :(
+      // this.$nextTick(() => {
+      //   this.handleResize();
+      // });
+    }
+    window.addEventListener("resize", this.handleResize);
+  },
+  methods: {
+    handleResize() {
+      this.screenCanScroll = document.body.clientHeight > screen.height;
+    },
+  },
+};
 </script>
+
+<style scoped>
+@media (min-height: 1175px) {
+  section {
+    @apply pb-24;
+  }
+}
+</style>
