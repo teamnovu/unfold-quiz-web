ba953a5fed525fb5228c45045712c3effcb91d5a
theLeroy <janis.wanger@novu.ch>
2022-03-30 17:50:52 +0200

    Update Container.vue

--- components/general/Container.vue
+++ components/general/Container.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="px-9 pt-28 pb-10 sm:pl-28 sm:pr-40 sm:pt-44 ">
+  <section class="px-9 pt-24 pb-10 sm:pl-28 sm:pr-40 sm:pt-40 ">
     <slot />
   </section>
 </template>
